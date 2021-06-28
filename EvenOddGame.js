6817d881576fa1642f7c02358a2501a417691e26
EtielYakira <that1234@gmail.com>
2021-06-28 09:36:49 +0300

    removing logs

--- EvenOddGame.js
+++ EvenOddGame.js
@@ -1,13 +1,10 @@
 const prompt = require('prompt-sync')()
 const chalk = require('chalk');
 
-console.log(chalk.rgb(100,200,100)('Ahalan'));
-
-console.log();
 
 const playerOneName = prompt('player '+ chalk.red(1) +' name: ')
 const playerTwoName = prompt(chalk.underline('player '+ chalk.green(2) +' name: '))
 
 
-console.log(chalk.bgGreen(`hey ${playerOneName}`))
+console.log(`hey ${playerOneName}`)
 console.log(`hey ${playerTwoName}`)
